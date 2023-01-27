23394af87732b7bbfdea586fa4e140f2dd01ab55
easrng <easrng@gmail.com>
2023-01-27 17:51:02 -0500

    Update gen.js

--- gen.js
+++ gen.js
@@ -2,7 +2,9 @@ const allThePackageNames = require("all-the-package-names")
 const pnpm = require('path').dirname(require.resolve("pnpm"))+"/dist/pnpm.cjs"
 const exec = require('util').promisify(require('child_process').execFile)
 let i = 0;
+(async()=>{
 for(let pkg of allThePackageNames) {
   console.log("installing package",++i,"of",allThePackageNames.length)
   await exec(pnpm, ['install',pkg])
 }
+})()
