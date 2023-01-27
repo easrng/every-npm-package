3ca176f356f2c11d6e2ec5245837ec4157589578
easrng <me@easrng.net>
2023-01-27 17:14:04 -0500

    awa

--- gen.js
+++ gen.js
@@ -1,13 +1,19 @@
 const package = {
-  "name": "generated",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
+  name: "generated",
+  version: "1.0.0",
+  description: "",
+  main: "index.js",
+  scripts: {
+    test: 'echo "Error: no test specified" && exit 1',
   },
-  "author": "",
-  "license": "UNLICENSED"
+  author: "",
+  license: "UNLICENSED",
+};
+package.dependencies = Object.fromEntries(
+  require("all-the-package-names").map((e) => [e, "*"])
+);
+const fs = require("fs");
+if (!fs.existsSync("generated")) {
+  fs.mkdirSync("generated");
 }
-package.dependencies=Object.fromEntries(require("all-the-package-names").map(e=>[e,"*"]))
-require('fs').writeFileSync('generated/package.json',(JSON.stringify(package)))
+fs.writeFileSync("generated/package.json", JSON.stringify(package));
